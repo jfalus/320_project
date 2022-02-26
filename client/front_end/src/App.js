82c650ead43d4e87d34c8e2d051c20d354117048
khuongVu01 <vuduykhuong01@gmail.com>
2022-02-26 17:47:42 -0500

    Backbone react-collapsed component

--- client/front_end/src/App.js
+++ client/front_end/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import useCollapse from "react-collapsed";
+import "./App.css";
+function Section(props) {
+  const config = {
+    defaultExpanded: props.defaultExpanded || false,
+    collapsedHeight: props.collapsedHeight || 0,
+  };
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse(config);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="collapsible">
+      <div className="header" {...getToggleProps()}>
+        <div className="title">{props.title}</div>
+        <div className="icon">
+          <i
+            className={"fas fa-chevron-circle-" + (isExpanded ? "up" : "down")}
+          ></i>
+        </div>
+      </div>
+      <div {...getCollapseProps()}>
+        <div className="content">{props.children}</div>
+      </div>
     </div>
   );
 }
-
+function App() {
+  return (
+    <div className="preferences">
+      <Section title="Personal time off request">
+        <label>
+          <input type="checkbox" /> Due date: N/A
+        </label>
+        <br />
+      </Section>
+      <Section title="To do 2">
+        <label>
+          <input type="checkbox" /> Due date: N/A
+        </label>
+      </Section>
+      <Section title="To do 3" collapsedHeight="32"></Section>
+    </div>
+  );
+}
 export default App;
