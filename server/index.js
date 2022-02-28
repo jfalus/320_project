e14d625cedac97e8c61b96aaf65d9ec8c70dba80
Ian Davis <iandavis742@gmail.com>
2022-02-28 15:21:18 -0500

    package.json scripts

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile('joenjoe.png', {root: '.'})
+  res.sendFile('joenjoe.png', {root: './server'})
 })
 
 app.get('/hello', (req, res) => {
