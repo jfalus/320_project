597483b8168ebb6b440683e6eefb217d48cf33f2
Bill <bchen434@gmail.com>
2022-03-09 00:07:36 -0500

    moved endpoints and added tests for passport

--- __tests_server__/index.test.js
+++ __tests_server__/index.test.js
@@ -13,12 +13,36 @@ describe("Test the root path", () => {
 });
 
 describe("Test /hello", () => {
-    test("It should response the GET method", done => {
-      request(app)
-        .get("/hello")
-        .then(response => {
-          expect(response.text).toBe("hello world!");
-          done();
-        });
-    });
+  test("It should response the GET method", done => {
+    request(app)
+      .get("/hello")
+      .then(response => {
+        expect(response.text).toBe("hello world!");
+        done();
+      });
   });
+});
+
+describe("Test correct login", () => {
+  test("Login should be a success", done => {
+    request(app)
+      .post("/login")
+      .send({username:"email1", password:"password1"})
+      .then(response => {
+        expect(response.text).toBe("Found. Redirecting to /home");
+        done();
+      });
+  });
+});
+
+describe("Test incorrent login", () => {
+  test("Login should fail", done => {
+    request(app)
+      .post("/login")
+      .send({username:"email1", password:"password2"})
+      .then(response => {
+        expect(response.text).toBe("Found. Redirecting to /");
+        done();
+      });
+  });
+});
