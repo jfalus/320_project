1b617dd19458cf378f2f5ab5e88e4219a6db4b8f
sid <sid.dev2000@protonmail.com>
2022-03-22 22:24:36 -0400

    created login-page

--- client/front_end/src/App.js
+++ client/front_end/src/App.js
@@ -1,32 +1,20 @@
 import React from "react";
-import './index.css'
-import Navbar from './Navbar.js'
-import logo from "./logo.svg";
-import "./App.css";
-import "./style.css";
-import ToolBar from "./ToolBar";
-import Sidebar from "./Sidebar";
-import Section from "./components/collapsible_list";
-import {Route, BrowserRouter as Routes, BrowserRouter, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Home from "./pages/Home"
 import NewItem from "./NewItem"
-import Home from "./Home"
 import Settings from "./Settings";
 
-const App=()=>{
+const App = () => {
   return (
-    
-    <BrowserRouter>
-    <Routes>
-
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/create-item" component={NewItem}/>
-        <Route path="/settings" component={Settings}/>
-      </Switch>
-
-    </Routes>
-    </BrowserRouter>
-
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create-item" element={<NewItem />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
