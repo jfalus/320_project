195069369af1e985562a737f327c4a6115b39aff
khuongVu01 <vuduykhuong01@gmail.com>
2022-03-24 20:22:51 -0400

    create modal

--- client/front_end/src/components/Sidebar.js
+++ client/front_end/src/components/Sidebar.js
@@ -1,71 +1,84 @@
 import React from "react";
 import "../styles/Sidebar.css";
+import NewItem from "./createItem";
 
-const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <li>
-        <button type="button">Create Item</button>
-      </li>
+export default class Sidebar extends React.Component {
+  state = {
+    seen: false,
+  };
+  togglePop = () => {
+    this.setState({
+      seen: !this.state.seen,
+    });
+  };
 
-      <li>
-        <h2>
-          <b>Categories</b>
-        </h2>
-        <br></br>
-        <br></br>
-
-        <body2>
-          <a href="#" class="notification">
-            <span>PTO Requests</span>
-            <span class="badge">3</span>
-          </a>
-          <br></br>
-          <br></br>
-
-          <a href="#" class="notification">
-            <span>Performance Reviews</span>
-            <span class="badge">1</span>
-          </a>
-          <br></br>
-          <br></br>
-
-          <a href="#" class="notification">
-            <span>Trainings</span>
-            <span class="badge">4</span>
-          </a>
-          <br></br>
-        </body2>
-      </li>
-
-      <li>
-        <div class="dropdown">
-          <button class="dropbtn">
-            Filters <i class="fa fa-caret-down" aria-hidden="true"></i>
-          </button>
-
-          <div class="dropdown-content">
-            <a target="_blank" href="">
-              TODO
-            </a>
-            <a target="_blank" href="">
-              IN PROGRESS
-            </a>
-            <a target="_blank" href="">
-              COMPLETED
-            </a>
+  render() {
+    return (
+      <div className="sidebar">
+        <li>
+          <div className="button" onClick={this.togglePop}>
+            <button>Create Task</button>
           </div>
+          {this.state.seen ? <NewItem toggle={this.togglePop} /> : null}
+        </li>
+
+        <li>
+          <h2>
+            <b>Categories</b>
+          </h2>
+          <br></br>
+          <br></br>
+
+          <body2>
+            <a href="#" class="notification">
+              <span>PTO Requests</span>
+              <span class="badge">3</span>
+            </a>
+            <br></br>
+            <br></br>
+
+            <a href="#" class="notification">
+              <span>Performance Reviews</span>
+              <span class="badge">1</span>
+            </a>
+            <br></br>
+            <br></br>
+
+            <a href="#" class="notification">
+              <span>Trainings</span>
+              <span class="badge">4</span>
+            </a>
+            <br></br>
+          </body2>
+        </li>
+
+        <li>
+          <div class="dropdown">
+            <button class="dropbtn">
+              Filters <i class="fa fa-caret-down" aria-hidden="true"></i>
+            </button>
+
+            <div class="dropdown-content">
+              <a target="_blank" href="">
+                TODO
+              </a>
+              <a target="_blank" href="">
+                IN PROGRESS
+              </a>
+              <a target="_blank" href="">
+                COMPLETED
+              </a>
+            </div>
+          </div>
+        </li>
+
+        <div id="settings">
+          SETTINGS{" "}
+          <a href="google.com">
+            <i class="fa fa-cog fa-3x" aria-hidden="true"></i>
+          </a>
         </div>
-      </li>
-
-      <div id="settings">
-        SETTINGS{" "}
-        <a href="google.com">
-          <i class="fa fa-cog fa-3x" aria-hidden="true"></i>
-        </a>
       </div>
-    </div>
-  );
-};
-
-export default Sidebar;
+    );
+  }
+}
