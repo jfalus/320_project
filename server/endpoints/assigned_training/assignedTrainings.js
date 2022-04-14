c52dd92d15958216da56c7210a219a2e38d0947a
Ryan Fletcher <ryan.fletcher.9999@gmail.com>
2022-04-13 22:55:51 -0400

    v0.1 of UPDATE endpoint fixes and integration

--- server/endpoints/assigned_training/assignedTrainings.js
+++ server/endpoints/assigned_training/assignedTrainings.js
@@ -2,16 +2,16 @@ const checkLoggedIn = require('../authentication/checkLoggedIn');
 const {models} = require('../../sequelize/sequelizeConstructor');
 /* NEED TO DECIDE WHO CAN VIEW/EDIT WHOSE TASKS */
 
-// GET /api/empTasks/assignedTrainings?EID=aBigInt
+// GET /api/empTasks/assignedTrainings?ASTO=aBigInt
 // Passes a json file with the employee's assigned trainings
 function assignedTrainings(app){
   app.get('/api/empTasks/assignedTrainings',
   checkLoggedIn,
   async (req, res) => {
     const assigned_trainings = await models.assigned_training.findAll({
-      attributes: ['title', 'description', 'link', 'date_created', 'date_due', 'progress'/**/],
+      attributes: ['at_id', 'title', 'description', 'link', 'date_created', 'date_due', 'progress', 'assigned_to'],
       where: {
-        e_id: req.query.EID
+        assigned_to: req.query.ASTO
       }
     });
     res.json(assigned_trainings)
--- server/endpoints/pto_request/ptoRequests.js
+++ server/endpoints/pto_request/ptoRequests.js
@@ -1,16 +1,16 @@
 const checkLoggedIn = require('../authentication/checkLoggedIn');
 const {models} = require('../../sequelize/sequelizeConstructor');
 
-// GET /api/empTasks/ptoRequests?EID=aBigInt
+// GET /api/empTasks/ptoRequests?ASTO=aBigInt
 // Passes a json file with the employee's pto requests
 function ptoRequests(app){
   app.get('/api/empTasks/ptoRequests',
   checkLoggedIn,
   async (req, res) => {
     const pto_requests = await models.pto_request.findAll({
-      attributes: ['title', 'description', 'start_date', 'end_date', 'date_created', 'date_due', 'progress', 'approved', 'assigned_to'],
+      attributes: ['pto_id', 'title', 'description', 'start_date', 'end_date', 'date_created', 'date_due', 'progress', 'approved', 'assigned_to'],
       where: {
-        e_id: req.query.EID
+        assigned_to: req.query.ASTO
       }
     });
     res.json(pto_requests)
