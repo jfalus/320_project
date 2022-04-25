caa7e4a861ac7a4f030ee952782a6cc681485ea6
Bill <bchen434@gmail.com>
2022-04-25 19:27:38 -0400

    removed proxy

--- client/front_end/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:5000" })
-  );
-};
