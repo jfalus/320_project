e9cf92864f3ca0f746d5c02021b9b5d399ba6d67
sid <sid.dev2000@protonmail.com>
2022-05-04 15:07:36 -0400

    store notification count when categories toggle

--- client/front_end/src/pages/Home.js
+++ client/front_end/src/pages/Home.js
@@ -13,16 +13,18 @@ class Home extends Component {
       category: "",
       progress: "",
       sort: "",
+      totalCount: []
     }
-  this.getAllTasksSmooth = this.getAllTasksSmooth.bind(this);
-  this.handleSearchChange = this.handleSearchChange.bind(this);
-  this.updateCategory = this.updateCategory.bind(this);
-  this.updateFilter = this.updateFilter.bind(this);
-  this.updateSort = this.updateSort.bind(this);
+    this.getAllTasksSmooth = this.getAllTasksSmooth.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.updateCategory = this.updateCategory.bind(this);
+    this.updateFilter = this.updateFilter.bind(this);
+    this.updateSort = this.updateSort.bind(this);
   }
 
   async componentDidMount() {
-    this.getAllTasksSmooth(true, undefined, false);
+    await this.getAllTasksSmooth(true, undefined, false);
+    this.setState({ totalCount: ["Paid Time Off Request", "Performance Review", "Assigned Training", "General Task"].map(e => (this.filterTasks(this.state.tasks, "category", e).length)) })
     // if (false) {
     //   this.pushtask({
     //     "category":"Paid Time Off Request",
@@ -41,34 +43,32 @@ class Home extends Component {
 
   // Accesses a GET endpoint for the current user, returns array of JSON objects
   // ex: getKind("assignedTrainings")
-  async getKind(url_kind, request_options={method: 'GET'}, debug=false)
-  {
+  async getKind(url_kind, request_options = { method: 'GET' }, debug = false) {
     var ret;
     await fetch("/api/empTasks/" + url_kind, request_options)
-        .then(res => {
-          if (res.redirected) {
-            window.location.href = res.url;
-          }
-          return res;
-        })
-        .then(response => response.json())
-        .then(result => {
-          if(debug) {console.log(url_kind + " assigned to self:\n"); result.forEach(t => console.log(t));}
-          ret = result;
-        })
-        .catch(error => console.log('error', error));
+      .then(res => {
+        if (res.redirected) {
+          window.location.href = res.url;
+        }
+        return res;
+      })
+      .then(response => response.json())
+      .then(result => {
+        if (debug) { console.log(url_kind + " assigned to self:\n"); result.forEach(t => console.log(t)); }
+        ret = result;
+      })
+      .catch(error => console.log('error', error));
     return ret;
   }
 
   // Accesses all task GET endpoints for the current user, returns object: {assigned_trainings:[JSON objects], performance_reviews:[JSON objects], pto_requests:[JSON objects], general_tasks:[JSON objects]}
   // ex: getAllTasks()
-  async getAllTasks(request_options={method: 'GET'}, debug=false)
-  {
+  async getAllTasks(request_options = { method: 'GET' }, debug = false) {
     const ret = {};
     const tasks = await Promise.all([this.getKind("assignedTrainings", request_options, debug),
-      this.getKind("performanceReviews", request_options, debug),
-      this.getKind("ptoRequests", request_options, debug),
-      this.getKind("generalTasks", request_options, debug)]);
+    this.getKind("performanceReviews", request_options, debug),
+    this.getKind("ptoRequests", request_options, debug),
+    this.getKind("generalTasks", request_options, debug)]);
     ret.assigned_trainings = tasks[0] || [];
     ret.performance_reviews = tasks[1] || [];
     ret.pto_requests = tasks[2] || [];
@@ -81,60 +81,57 @@ class Home extends Component {
   }
 
   pushtask(element) {
-    this.setState({tasks: this.state.tasks.concat(element)})
+    this.setState({ tasks: this.state.tasks.concat(element) })
   }
 
   // Accesses all task GET endpoints for current user, returns singular array of JSON objects
   // ex: getAllTasksSmooth()
-  async getAllTasksSmooth(category_strings=false, request_options={method: 'GET'}, debug=false)
-  {
-    this.setState({tasks: []})
-    const tasks = await this.getAllTasks(request_options, debug=true);
-    tasks.assigned_trainings.forEach(e => {if(category_strings){e.category = "Assigned Training";} this.pushtask(e)});
-    tasks.performance_reviews.forEach(e => {if(category_strings){e.category = "Performance Review";} this.pushtask(e);});
-    tasks.pto_requests.forEach(e => {if(category_strings){e.category = "Paid Time Off Request";} this.pushtask(e);});
-    tasks.general_tasks.forEach(e => {if(category_strings){e.category = "General Task";} this.pushtask(e);});
+  async getAllTasksSmooth(category_strings = false, request_options = { method: 'GET' }, debug = false) {
+    this.setState({ tasks: [] })
+    const tasks = await this.getAllTasks(request_options, debug = true);
+    tasks.assigned_trainings.forEach(e => { if (category_strings) { e.category = "Assigned Training"; } this.pushtask(e) });
+    tasks.performance_reviews.forEach(e => { if (category_strings) { e.category = "Performance Review"; } this.pushtask(e); });
+    tasks.pto_requests.forEach(e => { if (category_strings) { e.category = "Paid Time Off Request"; } this.pushtask(e); });
+    tasks.general_tasks.forEach(e => { if (category_strings) { e.category = "General Task"; } this.pushtask(e); });
   }
 
   // Accesses directManagedEmployees endpoint (gets direct subordinates of current user), returns array of JSON objects
   // ex: getDirectSubordinateEmployees()
-  async getDirectSubordinateEmployees(request_options={method: 'GET'}, debug=false)
-  {
+  async getDirectSubordinateEmployees(request_options = { method: 'GET' }, debug = false) {
     var ret;
     await fetch("/api/directManagedEmployees", request_options)
-        .then(res => {
-          if (res.redirected) {
-            window.location.href = res.url;
-          }
-          return res;
-        })
-        .then(response => response.json())
-        .then(result => {
-          if(debug) {console.log("Direct Subordinate Employees:\n"); result.forEach(t => console.log(t));}
-          ret = result;
-        })
-        .catch(error => console.log('error', error));
+      .then(res => {
+        if (res.redirected) {
+          window.location.href = res.url;
+        }
+        return res;
+      })
+      .then(response => response.json())
+      .then(result => {
+        if (debug) { console.log("Direct Subordinate Employees:\n"); result.forEach(t => console.log(t)); }
+        ret = result;
+      })
+      .catch(error => console.log('error', error));
     return ret;
   }
 
   // Accesses allManagedEmployees endpoint (gets all subordinates of current user), returns array of JSON objects
   // ex: getAllSubordinateEmployees()
-  async getAllSubordinateEmployees(request_options={method: 'GET'}, debug=false)
-  {
+  async getAllSubordinateEmployees(request_options = { method: 'GET' }, debug = false) {
     var ret;
     await fetch("/api/allManagedEmployees", request_options)
-        .then(res => {
-          if (res.redirected) {
-            window.location.href = res.url;
-          }
-          return res;
-        })
-        .then(response => response.json())
-        .then(result => {
-          if(debug) {console.log("All Subordinate Employees:\n"); result.forEach(t => console.log(t));}
-          ret = result;
-        })
-        .catch(error => console.log('error', error));
+      .then(res => {
+        if (res.redirected) {
+          window.location.href = res.url;
+        }
+        return res;
+      })
+      .then(response => response.json())
+      .then(result => {
+        if (debug) { console.log("All Subordinate Employees:\n"); result.forEach(t => console.log(t)); }
+        ret = result;
+      })
+      .catch(error => console.log('error', error));
     return ret;
   }
 
@@ -149,8 +146,7 @@ class Home extends Component {
   // ##################################################################################################################
   // task_kind should be one of the above Strings.
   // bodyFields should be an array. Each element is another array of length 2, where element 0 is the name of the body field (String) and element 1 is the value of that body field.
-  async updateTask(task_kind, bodyFields, debug=false)
-  {
+  async updateTask(task_kind, bodyFields, debug = false) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     var urlencoded = new URLSearchParams();
@@ -165,24 +161,24 @@ class Home extends Component {
     };
     var ret;
     await fetch("/api/empTasks/update" + task_kind, request_options)
-        .then(res => {
-          if (res.redirected) {
-            window.location.href = res.url;
-          }
-          return res;
-        })
-        .then(response => response.text())
-        .then(result => {
-          if(debug) {console.log("Update " + task_kind + ":\n" + result);}
-          ret = result === "true";
-        })
-        .catch(error => console.log('error', error));
+      .then(res => {
+        if (res.redirected) {
+          window.location.href = res.url;
+        }
+        return res;
+      })
+      .then(response => response.text())
+      .then(result => {
+        if (debug) { console.log("Update " + task_kind + ":\n" + result); }
+        ret = result === "true";
+      })
+      .catch(error => console.log('error', error));
     this.getAllTasksSmooth(true, undefined, false);
     return ret;
   }
 
   handleSearchChange(event) {
-    this.setState({search: event.target.value});
+    this.setState({ search: event.target.value });
   }
 
   // Filters tasks according to some field of the json, such as progress or category
@@ -240,12 +236,12 @@ class Home extends Component {
   updateCategory(category) {
     if (this.state.category === category) {
       this.setState((state) => {
-        return {category: ""};
+        return { category: "" };
       });
     }
     else {
       this.setState((state) => {
-        return {category: category};
+        return { category: category };
       });
     }
   }
@@ -253,12 +249,12 @@ class Home extends Component {
   updateFilter(filter) {
     if (this.state.progress === filter) {
       this.setState((state) => {
-        return {progress: ""};
+        return { progress: "" };
       });
     }
     else {
       this.setState((state) => {
-        return {progress: filter};
+        return { progress: filter };
       });
     }
   }
@@ -267,94 +263,93 @@ class Home extends Component {
     if (this.state.sort === sort) {
       console.log("setting sort to none")
       this.setState((state) => {
-        return {sort: ""};
+        return { sort: "" };
       });
     }
     else {
       console.log("setting sort to " + sort);
       this.setState((state) => {
-        return {sort: sort};
+        return { sort: sort };
       });
     }
   }
 
-  render()
-  {
+  render() {
     let filteredTasks = this.applyFilters();
     return (
-        <>
-          <Header handler={this.handleSearchChange} sorter={this.updateSort}/>
-          <div style={{
-            contentDiv: {
-              display: "flex",
-            },
-            contentMargin: {
-              marginLeft: "0px",
-              width: "100%",
-              backgroundColor: "005151",
-            },
-          }.contentDiv}>
-            <Sidebar updateCategory={this.updateCategory} updateFilter={this.updateFilter} counts = {["Paid Time Off Request", "Performance Review", "Assigned Training", "General Task"].map(e => (this.filterTasks(filteredTasks, "category", e).length))}/>
-            <div className="Main-section">
-              {filteredTasks.map(e => {
-                if (e.category === "General Task") {
-                  return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
-                      category={e.category}
-                      title={e.title}
-                      dueDate={e.date_due}
-                      id={e.task_id}
-                      description={e.description}
-                      createdDate={e.date_created}
-                      progress={e.progress}
-                  />);
-                }
-                else if (e.category === "Assigned Training") {
-                  return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
-                      category={e.category}
-                      title={e.title}
-                      dueDate={e.date_due}
-                      id={e.at_id}
-                      link={e.link}
-                      createdDate={e.date_created}
-                      description={e.description}
-                      progress={e.progress}
-                  />);
-                }
-                else if (e.category === "Performance Review") {
-                  return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
-                      category={e.category}
-                      title={e.title}
-                      dueDate={e.date_due}
-                      id={e.pr_id}
-                      createdDate={e.date_created}
-                      overallcomments={e.overall_comments}
-                      growth_feedback={e.growth_feedback}
-                      kindness_feedback={e.kindness_feedback}
-                      delivery_feedback={e.delivery_feedback}
-                      progress={e.progress}
-                  />);
-                }
-                else if (e.category === "Paid Time Off Request") {
-                  return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
-                      category={e.category}
-                      title={e.title}
-                      dueDate={e.date_due}
-                      id={e.pto_id}
-                      createdDate={(e.date_created!=null)?e.date_created:"2022-01-01"}
-                      start_date={e.start_date}
-                      end_date={e.end_date}
-                      description={e.description}
-                      approved={(e.approved!=null)?e.approved:false}
-                      progress={e.progress}
-                      e_id={e.e_id}
-                  />);
-                }
-              })}
-            </div>
+      <>
+        <Header handler={this.handleSearchChange} sorter={this.updateSort} />
+        <div style={{
+          contentDiv: {
+            display: "flex",
+          },
+          contentMargin: {
+            marginLeft: "0px",
+            width: "100%",
+            backgroundColor: "005151",
+          },
+        }.contentDiv}>
+          <Sidebar updateCategory={this.updateCategory} updateFilter={this.updateFilter} counts={this.state.totalCount} />
+          <div className="Main-section">
+            {filteredTasks.map(e => {
+              if (e.category === "General Task") {
+                return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
+                  category={e.category}
+                  title={e.title}
+                  dueDate={e.date_due}
+                  id={e.task_id}
+                  description={e.description}
+                  createdDate={e.date_created}
+                  progress={e.progress}
+                />);
+              }
+              else if (e.category === "Assigned Training") {
+                return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
+                  category={e.category}
+                  title={e.title}
+                  dueDate={e.date_due}
+                  id={e.at_id}
+                  link={e.link}
+                  createdDate={e.date_created}
+                  description={e.description}
+                  progress={e.progress}
+                />);
+              }
+              else if (e.category === "Performance Review") {
+                return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
+                  category={e.category}
+                  title={e.title}
+                  dueDate={e.date_due}
+                  id={e.pr_id}
+                  createdDate={e.date_created}
+                  overallcomments={e.overall_comments}
+                  growth_feedback={e.growth_feedback}
+                  kindness_feedback={e.kindness_feedback}
+                  delivery_feedback={e.delivery_feedback}
+                  progress={e.progress}
+                />);
+              }
+              else if (e.category === "Paid Time Off Request") {
+                return (<Section updateTask={this.updateTask} getAllTasksSmooth={this.getAllTasksSmooth}
+                  category={e.category}
+                  title={e.title}
+                  dueDate={e.date_due}
+                  id={e.pto_id}
+                  createdDate={(e.date_created != null) ? e.date_created : "2022-01-01"}
+                  start_date={e.start_date}
+                  end_date={e.end_date}
+                  description={e.description}
+                  approved={(e.approved != null) ? e.approved : false}
+                  progress={e.progress}
+                  e_id={e.e_id}
+                />);
+              }
+            })}
           </div>
-        </>
+        </div>
+      </>
     );
   }
 }
 
-export {Home};
+export { Home };
